28bffa07746641dde196b4028a4e70595d9596aa
fabianvdW <fabianvonderwarth@gmail.com>
2017-09-08 17:00:42 +0200

    Hat den Anschein zu funktionieren. Schnittpunkte nochmal überprüfen

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,7 @@
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -9,30 +10,81 @@ public class  GUI extends JPanel {
     private static int height;
     private ArrayList<Strecke> strecken;
     private ArrayList<Schnittpunkt> schnittpunkte;
-    public GUI(ArrayList<Strecke> strecken,ArrayList<Schnittpunkt> schnittpunkte){
+    private ArrayList<Dreieck> dreiecke;
+    public GUI(ArrayList<Strecke> strecken,ArrayList<Schnittpunkt> schnittpunkte,ArrayList<Dreieck> dreiecke){
         this.strecken=strecken;
         this.schnittpunkte=schnittpunkte;
+        this.dreiecke=dreiecke;
     }
     @Override
     public void paintComponent(Graphics g) {
-        int k=1;
+        //int k=1;
         for(Strecke s: strecken){
-            System.out.println(k++);
+            //System.out.println(k++);
             g.drawLine((int)s.p1.x+400,(int)s.p1.y+400,(int)s.p2.x+400,(int)s.p2.y+400);
         }
         g.setColor(Color.RED);
+        HashMap<Double,Double> coords= new HashMap<Double, Double>();
         for(Schnittpunkt s: schnittpunkte){
+            for(Double key: coords.keySet()){
+                if(s.x==key &&coords.get(key)==s.y){
+                    System.out.println("WOWOWOW");
+                    g.setColor(Color.GREEN);
+                }
+            }
+            coords.put(s.x,s.y);
             g.drawOval((int) s.x + 398, (int) s.y + 398, 5, 5);
+            g.setColor(Color.RED);
 
         }
         g.setColor(Color.BLACK);
+        ArrayList<Color> colors= new ArrayList<Color>();
+        colors.add(new Color(10,10,210));
+        colors.add(new Color(10,10,230));
+        colors.add(new Color(10,10,10));
+        colors.add(new Color(30,10,10));
+        colors.add(new Color(50,10,10));
+        colors.add(new Color(70,10,10));
+        colors.add(new Color(90,10,10));
+        colors.add(new Color(110,10,10));
+        colors.add(new Color(130,10,10));
+        colors.add(new Color(150,10,10));
+        colors.add(new Color(170,10,10));
+        colors.add(new Color(190,10,10));
+        colors.add(new Color(210,10,10));
+        colors.add(new Color(230,10,10));
+        colors.add(new Color(250,100,10));
+        colors.add(new Color(250,10,100));
+        colors.add(new Color(250,210,10));
+        colors.add(new Color(250,10,210));
+        colors.add(new Color(250,110,210));
+        colors.add(new Color(70,70,70));
+        colors.add(new Color(150,100,200));
+        colors.add(new Color(10,10,50));
+        colors.add(new Color(10,10,70));
+        colors.add(new Color(10,10,90));
+        colors.add(new Color(10,10,110));
+        colors.add(new Color(10,10,130));
+        colors.add(new Color(10,10,150));
+        colors.add(new Color(10,10,170));
+        colors.add(new Color(10,10,190));
+        int index=0;
+        for(Dreieck d: dreiecke){
+            if(colors.size()>index) {
+                g.setColor(colors.get(index));
+            }
+            g.drawLine((int)(d.p1.x+400),(int)(d.p1.y+400),(int)(d.p2.x+400),(int)(d.p2.y+400));
+            g.drawLine((int)(d.p1.x+400),(int)(d.p1.y+400),(int)(d.p3.x+400),(int)(d.p3.y+400));
+            g.drawLine((int)(d.p3.x+400),(int)(d.p3.y+400),(int)(d.p2.x+400),(int)(d.p2.y+400));
+            index++;
+        }
 
     }
 
-    public static void startBaum(ArrayList<Strecke> strecken,ArrayList<Schnittpunkt> schnittpunkte) {
+    public static void startBaum(ArrayList<Strecke> strecken,ArrayList<Schnittpunkt> schnittpunkte,ArrayList<Dreieck> dreiecke) {
         //Sort Baum
         JFrame jFrame = new JFrame();
-        jFrame.add(new GUI(strecken,schnittpunkte));
+        jFrame.add(new GUI(strecken,schnittpunkte,dreiecke));
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         width = (int)screenSize.getWidth();
         height =(int) screenSize.getHeight();
--- src/Strecke.java
+++ src/Strecke.java
@@ -4,10 +4,32 @@ public class Strecke {
     double m;
     double b;
     double[] defbereich;
+    double[] wertebereich;
     public Strecke(Punkt p1, Punkt p2){
         this.p1=p1;
         this.p2=p2;
         this.defbereich= new double[2];
+        this.wertebereich= new double[2];
+        if(p1.y>p2.y){
+            wertebereich[0]=p2.y;
+            wertebereich[1]=p1.y;
+        }else{
+            wertebereich[1]=p2.y;
+            wertebereich[0]=p1.y;
+        }
+    }
+    public Strecke(Schnittpunkt p1, Schnittpunkt p2){
+        this.p1=new Punkt(p1.x,p1.y);
+        this.p2=new Punkt(p2.x,p2.y);
+        this.defbereich= new double[2];
+        this.wertebereich= new double[2];
+        if(p1.y>p2.y){
+            wertebereich[0]=p2.y;
+            wertebereich[1]=p1.y;
+        }else{
+            wertebereich[1]=p2.y;
+            wertebereich[0]=p1.y;
+        }
     }
     @Override
     public String toString(){
@@ -20,6 +42,7 @@ public class Strecke {
         System.out.println("__________________________________________________\n\n");
         if(this.m==s2.m) {
             //System.out.println("HILFE");
+            System.out.println("Steigung gleich");
             return null;
         }
         System.out.println(this.toString());
@@ -30,11 +53,11 @@ public class Strecke {
                 double yschnitt= s2.m*this.p1.x+s2.b;
                 double xschnitt=(yschnitt-s2.b)/(s2.m);;
                 if(s2.m==0){
-                    xschnitt=s2.defbereich[0];
+                    xschnitt=this.defbereich[0];
                 }
                 System.out.println("Xschnitt: "+xschnitt);
                 System.out.println("yschnitt: "+yschnitt);
-                if(s2.imDefBereich(xschnitt)){
+                if(s2.imDefBereich(xschnitt)&& this.imWerteBereich(yschnitt)){
                    System.out.println("Im Def-Bereich");
                     return new Punkt(xschnitt,yschnitt);
                 }
@@ -42,11 +65,11 @@ public class Strecke {
                 double yschnitt= this.m*s2.p1.x+this.b;
                 double xschnitt=(yschnitt-this.b)/(this.m);
                 if(this.m==0){
-                    xschnitt=this.defbereich[0];
+                    xschnitt=s2.defbereich[0];
                 }
                 System.out.println("xschnitt: "+xschnitt);
                 System.out.println("yschnitt: "+yschnitt);
-                if(imDefBereich(xschnitt)){
+                if(imDefBereich(xschnitt)&& s2.imWerteBereich(yschnitt)){
                     System.out.println("Im Def-Bereich");
                     return new Punkt(xschnitt,yschnitt);
                 }
@@ -56,7 +79,7 @@ public class Strecke {
             System.out.println("Xschnitt: "+xschnitt);
             double yschnitt=this.m*xschnitt+this.b;
             System.out.println("Yscnintt: "+yschnitt);
-            if(imDefBereich(xschnitt)){
+            if(imDefBereich(xschnitt)&& s2.imWerteBereich(yschnitt)){
                System.out.println("Im Def-Bereich.");
                 return new Punkt(xschnitt,yschnitt);
             }
@@ -66,5 +89,20 @@ public class Strecke {
     }
     public boolean imDefBereich(double test){
         return test>=defbereich[0]&& test<=defbereich[1];
+    }
+    public boolean imWerteBereich(double test){
+        return test>=wertebereich[0]&& test<=wertebereich[1];
+    }
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof Strecke){
+            Strecke s= (Strecke) o;
+            return this.p1.x== s.p1.x&& this.p1.y== s.p1.y && this.p2.x== s.p2.x && this.p2.y==s.p2.y;
+        }else{
+            return false;
+        }
+    }
+    public double getLength(){
+        return Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2));
     }
 }
