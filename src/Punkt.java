1db59b63ba59acbb4ff92349d5713eafa873e163
Agent77326 <Agent77326@users.noreply.github.com>
2017-09-13 14:36:38 +0200

    Autoformat

--- src/Punkt.java
+++ src/Punkt.java
@@ -1,8 +1,9 @@
 public class Punkt {
     double x;
     double y;
-    public Punkt(double x, double y){
-        this.x=x;
-        this.y=y;
+
+    public Punkt(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 }
